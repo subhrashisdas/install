536a1d931a3a8f4dcdfc4231a2bfc520a2169d5c
Subhrashis Das <das.subhrashis@gmail.com>
2020-03-04 22:49:17 +0530

    Update systemd.sh

--- systemd.sh
+++ systemd.sh
@@ -1,1 +1,19 @@
+# Reference: https://vic.demuzere.be/articles/using-systemd-user-units/
 
+mkdir -p ~/.config/systemd/user
+touch ~/.config/systemd/user/metabase.service
+
+# systemctl --user start metabase
+# systemctl --user stop metabase
+# systemctl --user restart metabase
+# systemctl --user status metabase
+# loginctl enable-linger USERNAME
+# loginctl user-status USERNAME
+
+[Service]
+ExecStart=/usr/bin/env {{HOME}}/lib/java/bin/java -jar {{HOME}}/lib/metabase.jar
+RestartSec=10s
+Restart=on-failure
+
+[Install]
+WantedBy=default.target
