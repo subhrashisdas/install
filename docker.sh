18c2bc9271eecbe5e424c15b2e7c1cd5916304e8
Subhrashis Das <das.subhrashis@gmail.com>
2020-03-05 00:28:02 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,7 @@
+curl wget https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_18.09.0~3-0~ubuntu-bionic_amd64.deb > docker.deb
+sudo apt-get install libltdl7
+sudo dpkg -i docker.deb
+
+https://docs.docker.com/install/linux/docker-ce/ubuntu/
+https://upcloud.com/community/tutorials/how-to-configure-docker-swarm/
+https://docs.docker.com/install/linux/linux-postinstall/
