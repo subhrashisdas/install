184918ef1ea834bc35aa0c025a525c1b2b87e864
Subhrashis Das <das.subhrashis@gmail.com>
2020-03-05 01:32:08 +0530

    Update hasura.sh

--- hasura.sh
+++ hasura.sh
@@ -1,4 +1,5 @@
 docker run -d -p 8080:8080 \
   -e HASURA_GRAPHQL_DATABASE_URL=postgres://username:password@hostname:port/dbname \
   -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+  -e HASURA_GRAPHQL_ADMIN_SECRET=DriBhQmDsWa11dUyEFF09b9NypWFhBt1 \
   hasura/graphql-engine:latest
