eae7a57f880dba5efd0596c1802f0fe7434bcee2
Subhrashis Das <das.subhrashis@gmail.com>
2020-04-01 16:45:31 +0530

    Update postgresql-to-json.sql

--- postgresql-to-json.sql
+++ postgresql-to-json.sql
@@ -1,1 +1,14 @@
-
+SELECT coalesce(json_agg("root"), '[]') AS "root"
+FROM (
+    SELECT *
+    FROM box
+    LEFT OUTER JOIN LATERAL(
+        SELECT coalesce(json_agg("items".z), '[]') AS "items" FROM (
+            SELECT row_to_json(x) AS z
+            FROM (
+                SELECT *
+                FROM item
+                WHERE box.id = item.box_id
+            ) AS "x"
+        ) AS "items") AS z ON ('true')
+    ) AS "root";
