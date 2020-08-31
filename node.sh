27e0f10e565dc023ccbdda95b7b6d8aeee45b975
Subhrashis Das <das.subhrashis@gmail.com>
2020-09-01 00:57:48 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,6 @@
 # Install node.js
 
-NODE_VERSION=13.10.1;
+NODE_VERSION=14.9.0;
 mkdir -p ~/lib/node
 curl https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz | tar --strip-components=1 -xJvf - -C ~/lib/node
 echo 'export PATH=~/lib/node/bin:$PATH' >> ~/.profile
