d8169ed002c5c87426462f134cff910f4cf6616b
Subhrashis Das <das.subhrashis@gmail.com>
2021-04-26 16:15:49 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,6 +1,7 @@
 # Install node.js
 
 NODE_VERSION=15.1.0;
-mkdir -p ~/lib/node
-curl https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz | tar --strip-components=1 -xJvf - -C ~/lib/node
-echo 'export PATH=~/lib/node/bin:$PATH' >> ~/.profile
+rm -rf ~/lib/node;
+mkdir -p ~/lib/node;
+curl https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz | tar --strip-components=1 -xJvf - -C ~/lib/node;
+echo 'export PATH=~/lib/node/bin:$PATH' >> ~/.profile;
